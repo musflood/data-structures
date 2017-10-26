3a6c024e03803d378ed53f900cf7f992bbc4a03d
Megan Flood <mak.flood@comcast.net>
2017-10-26 16:32:29 -0700

    Fixed typo Misspelled the que_ module.

--- src/test_que_.py
+++ src/test_que_.py
@@ -12,7 +12,7 @@ def test_empty_queue_constructor(empty_queue):
 
 def test_queue_constructor_with_empty_iterable():
     """Test that a Queue created with an empty iterable is empty."""
-    from queue import Queue
+    from que_ import Queue
     q = Queue([])
     assert q._values.head is None
     assert q._values.tail is None
@@ -22,7 +22,7 @@ def test_queue_constructor_with_empty_iterable():
 @pytest.mark.parametrize('itr', [[x for x in range(y)] for y in range(1, 20)])
 def test_queue_constructor_with_iterable(itr):
     """Test that a queue created with an iterable contains all items."""
-    from queue import Queue
+    from que_ import Queue
     q = Queue(itr)
     assert q._values.head.val == itr[-1]
     assert q._values.tail.val == itr[0]
