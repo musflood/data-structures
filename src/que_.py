5aa42d11c92abd96b61242859acb8b656671406f
Megan Flood <mak.flood@comcast.net>
2017-10-26 18:15:04 -0700

    Dequeue and Peek for Queue Added dequeue and peek methods to the Queue. Also added the front and back properties to avoid confusion. Fixed a typo in DLL.

--- src/que_.py
+++ src/que_.py
@@ -12,5 +12,28 @@ class Queue(object):
         self._values = DLL(iterable)
 
     def enqueue(self, val):
-        """Add a value to the queue."""
+        """Add a value to the end of the queue."""
         self._values.push(val)
+
+    def dequeue(self):
+        """Remove the value from the front of the queue."""
+        try:
+            return self._values.shift()
+        except IndexError:
+            raise IndexError('Cannot dequeue from empty queue.')
+
+    def peek(self):
+        """Get the value from the front of the queue without removing it."""
+        if self._values.tail:
+            return self._values.tail.val
+        return None
+
+    @property
+    def front(self):
+        """Get the node at front of the queue."""
+        return self._values.tail
+
+    @property
+    def back(self):
+        """Get the node at back of the queue."""
+        return self._values.head
