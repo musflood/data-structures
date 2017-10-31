9b23011c3813ceecaab9eccd5c7054b3e64a4f0a
Megan Flood <mak.flood@comcast.net>
2017-10-31 16:20:54 -0700

    More push tests Added tests for pushing multiple values and duplicate values. Also fixed the parent/child tests.

--- src/test_binheap.py
+++ src/test_binheap.py
@@ -13,22 +13,25 @@ def test_binheap_constructed_with_no_arguments_is_empty(empty_binheap):
                                          (5, 2), (6, 3), (7, 3)])
 def test_find_parent_of_random_node_in_binheap(idx, result):
     """Test that _pi returns the parent of selected index."""
-    from binheap import _pi
-    assert _pi(idx) == result
+    from binheap import BinHeap
+    h = BinHeap()
+    assert h._pi(idx) == result
 
 
 @pytest.mark.parametrize('idx, result', [(1, 2), (2, 4), (3, 6)])
 def test_find_left_child_of_random_node_in_binheap(idx, result):
     """Test that _lci returns the left child of selected index."""
-    from binheap import _lci
-    assert _lci(idx) == result
+    from binheap import BinHeap
+    h = BinHeap()
+    assert h._lci(idx) == result
 
 
 @pytest.mark.parametrize('idx, result', [(1, 3), (2, 5), (3, 7)])
 def test_find_right_child_of_random_node_in_binheap(idx, result):
     """Test that _rci returns the right child of selected index."""
-    from binheap import _rci
-    assert _rci(idx) == result
+    from binheap import BinHeap
+    h = BinHeap()
+    assert h._rci(idx) == result
 
 
 def test_push_one_value_adds_value_to_empty_binheap(empty_binheap):
@@ -38,10 +41,35 @@ def test_push_one_value_adds_value_to_empty_binheap(empty_binheap):
     assert h._values[1] == 1
 
 
+def test_push_two_values_maintains_the_heap_property(empty_binheap):
+    """Test that adding two values will keep heap properties."""
+    h = empty_binheap
+    h.push(1)
+    h.push(2)
+    assert len(h._values) - 1 == 2
+    assert h._values[1] == 2
+    assert h._values[2] == 1
+
+
 def test_push_multiple_values_maintains_the_heap_property(empty_binheap):
     """Test that adding multiple values will keep heap properties."""
     h = empty_binheap
     h.push(1)
+    h.push(2)
+    h.push(3)
+    h.push(4)
+    assert len(h._values) - 1 == 4
+    assert h._values[1] == 4
+    assert h._values[2] == 3
+    assert h._values[3] == 2
+    assert h._values[4] == 1
+
+
+def test_push_non_unique_values_does_not_add_to_heap(empty_binheap):
+    """Test that duplicate values are not added to heap."""
+    h = empty_binheap
+    h.push(1)
+    h.push(2)
     h.push(2)
     assert len(h._values) - 1 == 2
     assert h._values[1] == 2
