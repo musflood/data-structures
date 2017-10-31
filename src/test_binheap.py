478e83fa7844060c2561432affca95b0983a3696
kavdihodgson <kavdyjh@gmail.com>
2017-10-31 15:56:53 -0700

    Test for adding values with push to binheap. Test that binheap maintains heap property.

--- src/test_binheap.py
+++ src/test_binheap.py
@@ -13,19 +13,36 @@ def test_binheap_constructed_with_no_arguments_is_empty(empty_binheap):
                                          (5, 2), (6, 3), (7, 3)])
 def test_find_parent_of_random_node_in_binheap(idx, result):
     """Test that _pi returns the parent of selected index."""
-    from binheap import BinHeap
-    assert BinHeap._pi(idx) == result
+    from binheap import _pi
+    assert _pi(idx) == result
 
 
 @pytest.mark.parametrize('idx, result', [(1, 2), (2, 4), (3, 6)])
 def test_find_left_child_of_random_node_in_binheap(idx, result):
     """Test that _lci returns the left child of selected index."""
-    from binheap import BinHeap
-    assert BinHeap._lci(idx) == result
+    from binheap import _lci
+    assert _lci(idx) == result
 
 
 @pytest.mark.parametrize('idx, result', [(1, 3), (2, 5), (3, 7)])
 def test_find_right_child_of_random_node_in_binheap(idx, result):
     """Test that _rci returns the right child of selected index."""
-    from binheap import BinHeap
-    assert BinHeap._rci(idx) == result
+    from binheap import _rci
+    assert _rci(idx) == result
+
+
+def test_push_one_value_adds_value_to_empty_binheap(empty_binheap):
+    """Test that adding a value to empty binheap returns binheap with value."""
+    h = empty_binheap
+    h.push(1)
+    assert h._values[1] == 1
+
+
+def test_push_multiple_values_maintains_the_heap_property(empty_binheap):
+    """Test that adding multiple values will keep heap properties."""
+    h = empty_binheap
+    h.push(1)
+    h.push(2)
+    assert len(h._values) - 1 == 2
+    assert h.values[1] == 2
+    assert h.values[2] == 1
