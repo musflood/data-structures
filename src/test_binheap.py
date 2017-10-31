8abc063c20f8102275a9b7fd2c2b97d7d18aac08
kavdihodgson <kavdyjh@gmail.com>
2017-10-31 15:27:46 -0700

    added test for finding right child and left child of value in binheap.

--- src/test_binheap.py
+++ src/test_binheap.py
@@ -15,3 +15,17 @@ def test_find_parent_of_random_node_in_binheap(idx, result):
     """Test that _pi returns the parent of selected index."""
     from binheap import BinHeap
     assert BinHeap._pi(idx) == result
+
+
+@pytest.mark.parametrize('idx, result', [(1, 2), (2, 4), (3, 5)])
+def test_find_left_child_of_random_node_in_binheap(idx, result):
+    """Test that _lci returns the left child of selected index."""
+    from binheap import BinHeap
+    assert BinHeap._lci(idx) == result
+
+
+@pytest.mark.parametrize('idx, result', [(1, 3), (2, 5), (3, 7)])
+def test_find_right_child_of_random_node_in_binheap(idx, result):
+    """Test that _rci returns the right child of selected index."""
+    from binheap import BinHeap
+    assert BinHeap._rci(idx) == result
