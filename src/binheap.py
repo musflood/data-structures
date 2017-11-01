f24e9451112ae0eeb654109e2a78d3cc855b25c9
Megan Flood <mak.flood@comcast.net>
2017-11-01 13:34:04 -0700

    Implemented min heap Added a compare function to allow the user to choose a min heap or max heap when instantiating a new heap.

--- src/binheap.py
+++ src/binheap.py
@@ -5,15 +5,22 @@ class BinHeap(object):
     """Structure for values in a max binary heap.
 
     A max binary heap is a complete binary tree where each level of the
-    tree is greater than the level below it.
+    tree is greater than the level below it. A min heap has the lowest
+    values at the top.
     """
 
-    def __init__(self, iterable=None):
+    def __init__(self, iterable=None, is_max_heap=True):
         """Construct a new binary heap."""
         self._values = [None]
+        self.is_max_heap = is_max_heap
         if isinstance(iterable, (str, list, tuple)):
             for item in iterable:
                 self.push(item)
+
+    @property
+    def _size(self):
+        """Get the number of items in the binary heap."""
+        return len(self._values) - 1
 
     def push(self, val):
         """Put a new value into the binary heap."""
@@ -23,7 +30,7 @@ class BinHeap(object):
         self._values.append(val)
 
         i = self._size
-        while self._pi(i) and self._values[self._pi(i)] < val:
+        while self._pi(i) and self._comp(val, self._values[self._pi(i)]):
             p = self._pi(i)
             self._values[p], self._values[i] = self._values[i], self._values[p]
             i = p
@@ -43,9 +50,9 @@ class BinHeap(object):
         lc = self._values[self._lci(i):self._lci(i) + 1]
         rc = self._values[self._rci(i):self._rci(i) + 1]
 
-        while (lc and lc[0] > x) or (rc and rc[0] > x):
+        while (lc and self._comp(lc[0], x)) or (rc and self._comp(rc[0], x)):
 
-            if rc and rc[0] > lc[0]:
+            if rc and self._comp(rc[0], lc[0]):
                 c = self._rci(i)
             else:
                 c = self._lci(i)
@@ -70,7 +77,8 @@ class BinHeap(object):
         """Find the index for the right child of the given index."""
         return idx * 2 + 1
 
-    @property
-    def _size(self):
-        """Get the number of items in the binary heap."""
-        return len(self._values) - 1
+    def _comp(self, val1, val2):
+        """Compare two values based on if the heap is max or min."""
+        if self.is_max_heap:
+            return val1 > val2
+        return val1 < val2
