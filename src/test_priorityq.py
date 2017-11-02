9d7f50fb0ee391c13a544d3bc30d42d0cd35eaa0
Megan Flood <mak.flood@comcast.net>
2017-11-02 14:08:00 -0700

    More insert tests Added more tests to check that insert works properly for a priority queue.

--- src/test_priorityq.py
+++ src/test_priorityq.py
@@ -71,7 +71,7 @@ def test_priorityq_constructed_with_no_arguments_is_empty(empty_priorityq):
     assert empty_priorityq._size == 0
 
 
-def test_insert_a_item_without_priority_adds_lowest_priority(empty_priorityq):
+def test_insert_an_item_without_priority_adds_lowest_priority(empty_priorityq):
     """Test inserting an item into the queue adds it as lowest priority."""
     q = empty_priorityq
     q.insert(5)
@@ -80,7 +80,7 @@ def test_insert_a_item_without_priority_adds_lowest_priority(empty_priorityq):
 
 
 def test_insert_items_without_priority_add_all_as_lowest(empty_priorityq):
-    """Test inserting an item into the queue adds it as lowest priority."""
+    """Test inserting items into the queue adds all as lowest priority."""
     q = empty_priorityq
     q.insert(5)
     q.insert(3)
@@ -88,3 +88,73 @@ def test_insert_items_without_priority_add_all_as_lowest(empty_priorityq):
     assert q._size == 1
     assert q._all_values[1].priority == 0
     assert len(q._all_values[1]) == 3
+
+
+def test_insert_an_item_with_priority_adds_at_that_priority(empty_priorityq):
+    """Test inserting an item into the queue adds it at given priority."""
+    q = empty_priorityq
+    q.insert(5, 6)
+    assert q._size == 1
+    assert q._all_values[1].priority == 6
+
+
+def test_insert_items_with_priority_add_all_at_that_priority(empty_priorityq):
+    """Test inserting items into the queue adds them at given priority."""
+    q = empty_priorityq
+    q.insert(5, 6)
+    q.insert(3, 6)
+    q.insert(1, 6)
+    assert q._size == 1
+    assert q._all_values[1].priority == 6
+    assert len(q._all_values[1]) == 3
+
+
+def test_insert_an_item_with_low_priority_changes_lowest(empty_priorityq):
+    """Test inserting an item into the queue can change min priority."""
+    q = empty_priorityq
+    q.insert(5, -6)
+    assert q._size == 1
+    assert q._min_priority == -6
+
+
+def test_insert_items_after_low_priority_add_to_same_priority(empty_priorityq):
+    """Test inserting an item into the queue can change min priority."""
+    q = empty_priorityq
+    q.insert(5, -6)
+    q.insert(3)
+    q.insert(8)
+    assert q._size == 1
+    assert len(q._all_values[1]) == 3
+
+
+def test_insert_items_with_diff_priority_order_by_priority(empty_priorityq):
+    """Test inserting items into the queue orders them by priority."""
+    q = empty_priorityq
+    q.insert(5, 6)
+    q.insert(3, 5)
+    q.insert(1, 10)
+    assert q._size == 3
+    assert q._all_values == [None, 10, 5, 6]
+
+
+def test_insert_items_with_diff_priority_group_by_priority(empty_priorityq):
+    """Test inserting items into the queue groups them by priority."""
+    q = empty_priorityq
+    q.insert(5, 2)
+    q.insert(3, 5)
+    q.insert(1, 2)
+    assert q._size == 2
+    assert q._all_values == [None, 5, 2]
+    assert len(q._all_values[2]) == 2
+
+
+def test_insert_items_with_same_priority_add_them_in_order(empty_priorityq):
+    """Test inserting items into the queue adds them at given priority."""
+    q = empty_priorityq
+    q.insert(5, 6)
+    q.insert(3, 6)
+    q.insert(1, 6)
+    assert q._size == 1
+    assert q._all_values[1]._values.dequeue() == 5
+    assert q._all_values[1]._values.dequeue() == 3
+    assert q._all_values[1]._values.dequeue() == 1
