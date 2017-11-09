7736349f5abfbcbcf6cba5f007544e43169a9794
Megan Flood <mak.flood@comcast.net>
2017-11-08 16:26:14 -0800

    Added most of the graph methods Added methods for adding nodes and edges, deleting nodes and edges, and chicking if a node is in the graph.

--- src/weight_graph.py
+++ src/weight_graph.py
@@ -13,7 +13,7 @@ class Graph(object):
         return list(self.graph)
 
     def edges(self):
-        """Get all edges in graph to display in list of tuples."""
+        """Get all edges in graph to display in list of tuples with weights."""
         edge_list = []
         for start in self.graph:
             for end in self.graph[start]:
@@ -22,26 +22,43 @@ class Graph(object):
 
     def add_node(self, val):
         """Add a node with a value to the graph."""
-        pass
+        self.graph.setdefault(val, {})
 
-    def add_edge(self, val1, val2):
-        """Add an edge with two values to the graph that does not exist."""
-        pass
+    def add_edge(self, val1, val2, weight):
+        """Add an edge from val1 to val2 with the given weight.
+
+        If the node for either value does not exist, it is added to the graph.
+        """
+        if val1 == val2:
+            raise ValueError('Edge needs two different values.')
+
+        self.add_node(val1)
+        self.add_node(val2)
+        self.graph[val1][val2] = weight
 
     def del_node(self, val):
         """Remove the node with the given value from the graph.
 
         Also removes all edges connected to the node.
         """
-        pass
+        if val not in self.graph:
+            raise ValueError('Value is not in the graph.')
+
+        del self.graph[val]
+        for node in self.graph:
+            if val in self.graph[node]:
+                del self.graph[node][val]
 
     def del_edge(self, val1, val2):
         """Remove the edge connecting node of val1 to node of val2."""
-        pass
+        try:
+            del self.graph[val1][val2]
+        except KeyError:
+            raise ValueError('Edge is not in the graph.')
 
     def has_node(self, val):
         """Check if the given value is in the graph."""
-        pass
+        return val in self.graph
 
     def neighbors(self, val):
         """List all nodes the node of the given value connects to."""
