0641fa4faec073a0aa54a1fadb20555fee2e224f
Megan Flood <mak.flood@comcast.net>
2017-12-09 19:28:28 -0800

    Completed bubble sort Made a bubble_sort function and tests for the function. Also added a name=main section to demonstrate the efficiency.

---
+++ src/bubble_sort.py
@@ -0,0 +1,59 @@
+"""Implements a bubble sorting algorithm.
+
+Bubble sort moves the largest values to the end of a list by
+iterating through the list repeatedly and swapping values if
+they are not in the correct order.
+"""
+
+
+def bubble_sort(values):
+    """Create a sorted copy of the given list using bubble sort."""
+    sort_list = values[:]
+    end = len(sort_list)
+
+    while end > 1:
+        for i in range(0, end - 1):
+            if sort_list[i] > sort_list[i + 1]:
+                sort_list[i], sort_list[i + 1] = sort_list[i + 1], sort_list[i]
+        end -= 1
+
+    return sort_list
+
+
+if __name__ == '__main__':  # pragma: no cover
+    from timeit import timeit
+    from random import randint
+    setup = 'from bubble_sort import bubble_sort'
+
+    print("""
+Bubble sort moves the largest values to the end of a list by
+iterating through the list repeatedly and swapping values if
+they are not in the correct order.
+""")
+
+    print('-- Best Case --')
+    for length in range(5, 20, 5):
+        l = sorted([randint(-100, 100) for _ in range(length)])
+
+        print("""Input: sorted([randint(-100, 100) for _ in range({})])
+    number of runs: 1000000""".format(len(l)))
+        time = timeit('bubble_sort({})'.format(l), setup)
+        print('    average time: {}ms\n'.format(time))
+
+    print('-- Average Case --')
+    for length in range(5, 20, 5):
+        l = [randint(-100, 100) for _ in range(length)]
+
+        print("""Input: [randint(-100, 100) for _ in range({})]
+    number of runs: 1000000""".format(len(l)))
+        time = timeit('bubble_sort({})'.format(l), setup)
+        print('    average time: {}ms\n'.format(time))
+
+    print('-- Worst Case --')
+    for length in range(5, 20, 5):
+        l = sorted([randint(-100, 100) for _ in range(length)], reverse=True)
+
+        print("""Input: sorted([randint(-100, 100) for _ in range({})], reverse=True)
+    number of runs: 1000000""".format(len(l)))
+        time = timeit('bubble_sort({})'.format(l), setup)
+        print('    average time: {}ms\n'.format(time))
