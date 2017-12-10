7d9cc1a63e1e2623bc8083e78aeedc69a2d50698
Megan Flood <mak.flood@comcast.net>
2017-12-09 20:02:33 -0800

    Optimized the bubble sort Can now exit the sorting if the list is already sorted.

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -10,11 +10,14 @@ def bubble_sort(values):
     """Create a sorted copy of the given list using bubble sort."""
     sort_list = values[:]
     end = len(sort_list)
+    is_sorted = False
 
-    while end > 1:
+    while end > 1 and not is_sorted:
+        is_sorted = True
         for i in range(0, end - 1):
             if sort_list[i] > sort_list[i + 1]:
                 sort_list[i], sort_list[i + 1] = sort_list[i + 1], sort_list[i]
+                is_sorted = False
         end -= 1
 
     return sort_list
